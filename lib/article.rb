f32eeffff7044446f707a845ed8aeaf7e74e7f07
wanja <114565394+WanjaNjunge@users.noreply.github.com>
2023-05-29 19:20:08 +0300

    added methods to the articles class

--- lib/article.rb
+++ lib/article.rb
@@ -1,5 +1,15 @@
 class Article
-    def initialize
-        
+    attr_reader :author, :magazine, :title
+    @@all = []
+
+    def initialize(author, magazine, title)
+        @author = author
+        @magazine = magazine
+        @title = title
+        @@all << self
+    end
+
+    def self.all
+        @@all
     end
 end
